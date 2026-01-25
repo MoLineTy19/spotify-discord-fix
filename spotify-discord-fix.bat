78d12e0724eb788d0b15af17965a273eafa81de5
MoLineTy19 <163237096+MoLineTy19@users.noreply.github.com>
2026-01-25 11:01:10 +0300

    Update spotify-discord-fix.bat
    updated ips

--- spotify-discord-fix.bat
+++ spotify-discord-fix.bat
@@ -1,6 +1,6 @@
 @echo off
 
-title spotify-discord-fix v1.0
+title spotify-discord-fix v1.01
 
 copy C:\Windows\System32\drivers\etc\hosts C:\hosts_backup.txt
 
@@ -8,20 +8,17 @@ copy C:\Windows\System32\drivers\etc\hosts C:\hosts_backup.txt
 echo.
 echo.
 echo # FIX-SPOTIFY v1.0
-echo 144.31.14.104 aet.spotify.com
-echo 45.155.204.190 accounts.scdn.co
-echo 45.155.204.190 accounts.spotify.com
-echo 45.155.204.190 api-partner.spotify.com
-echo 45.155.204.190 api.spotify.com
-echo 45.155.204.190 encore.scdn.co
-echo 45.155.204.190 gew1-dealer.spotify.com
-echo 45.155.204.190 gew1-spclient.spotify.com
-echo 45.155.204.190 login5.spotify.com
-echo 45.155.204.190 open-exp.spotifycdn.com
-echo 45.155.204.190 open.spotify.com
-echo 45.155.204.190 spclient.wg.spotify.com
-echo 45.155.204.190 www-growth.scdn.co
-echo 45.155.204.190 www.spotify.com
+134.0.118.88 gew1-spclient.spotify.com
+134.0.118.88 login5.spotify.com
+134.0.118.88 spotify.com
+134.0.118.88 api.spotify.com
+134.0.118.88 appresolve.spotify.com
+134.0.118.88 accounts.spotify.com
+134.0.118.88 aet.spotify.com
+134.0.118.88 open.spotify.com
+134.0.118.88 spotifycdn.com
+134.0.118.88 www.spotify.com
+134.0.118.88 spotifycdn.com
 echo # FIX SPOTIFY
 ) >> C:\Windows\System32\drivers\etc\hosts
 

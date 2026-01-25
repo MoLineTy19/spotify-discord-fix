cb3b8ce54e5cced909aada4eee0dd00a5a839f43
MoLineTy19 <163237096+MoLineTy19@users.noreply.github.com>
2026-01-25 17:24:59 +0300

    fix

--- spotify-discord-fix.bat
+++ spotify-discord-fix.bat
@@ -7,18 +7,18 @@ copy C:\Windows\System32\drivers\etc\hosts C:\hosts_backup.txt
 (
 echo.
 echo.
-echo # FIX-SPOTIFY v1.0
-134.0.118.88 gew1-spclient.spotify.com
-134.0.118.88 login5.spotify.com
-134.0.118.88 spotify.com
-134.0.118.88 api.spotify.com
-134.0.118.88 appresolve.spotify.com
-134.0.118.88 accounts.spotify.com
-134.0.118.88 aet.spotify.com
-134.0.118.88 open.spotify.com
-134.0.118.88 spotifycdn.com
-134.0.118.88 www.spotify.com
-134.0.118.88 spotifycdn.com
+echo # FIX-SPOTIFY v1.01
+echo 134.0.118.88 gew1-spclient.spotify.com
+echo 134.0.118.88 login5.spotify.com
+echo 134.0.118.88 spotify.com
+echo 134.0.118.88 api.spotify.com
+echo 134.0.118.88 appresolve.spotify.com
+echo 134.0.118.88 accounts.spotify.com
+echo 134.0.118.88 aet.spotify.com
+echo 134.0.118.88 open.spotify.com
+echo 134.0.118.88 spotifycdn.com
+echo 134.0.118.88 www.spotify.com
+echo 134.0.118.88 spotifycdn.com
 echo # FIX SPOTIFY
 ) >> C:\Windows\System32\drivers\etc\hosts
 
